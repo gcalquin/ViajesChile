8c6d91a249f78e66f2d8277311f5ce5433c87183
gcalquin <gcalquin@gmail.com>
2023-04-03 23:54:22 -0400

    Primer Commit

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+    
+
+    $('[data-toggle="popover"]').popover()
+    $("a").click(function (evento) { 
+        evento.preventDefault();
+        var gato= this.hash;
+        $("html").animate({
+            scrollTop: $(gato).offset().top,
+        }, 800)
+        
+    });
+});
+
+
+
+
+
